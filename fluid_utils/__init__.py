4278b9f3cd248b9bba38d6803459ae61f8e987a7
Probst <matthias.probst@kit.edu>
2022-06-08 14:01:28 +0200

    make __version__ available in __all__

--- fluid_utils/__init__.py
+++ fluid_utils/__init__.py
@@ -1,6 +1,9 @@
 from . import piv
 from ._logger import logger, _file_handler, _stream_handler
 
+from ._version import __version__
+
+__all__ = [__version__]
 
 def set_loglevel(level):
     logger.setLevel(level.upper())
